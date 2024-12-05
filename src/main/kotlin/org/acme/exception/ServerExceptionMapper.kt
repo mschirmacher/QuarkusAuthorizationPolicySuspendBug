f19493e9eaa7b1a496a7984a9d7c0eab87e4f192
Malte Schirmacher <schirmacher@mediamarktsaturn.com>
2024-12-05 16:26:24 +0100

    Also raise the priority

--- src/main/kotlin/org/acme/exception/ServerExceptionMapper.kt
+++ src/main/kotlin/org/acme/exception/ServerExceptionMapper.kt
@@ -1,12 +1,14 @@
 package org.acme.exception
 
 import com.fasterxml.jackson.core.JsonParseException
+import jakarta.annotation.Priority
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.ws.rs.core.Response
 import org.jboss.resteasy.reactive.ClientWebApplicationException
 import org.jboss.resteasy.reactive.server.ServerExceptionMapper
 
 @ApplicationScoped
+@Priority(1)
 class ServerExceptionMapper {
 
     @ServerExceptionMapper
