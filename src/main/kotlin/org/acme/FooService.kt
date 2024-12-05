7b919359ca1133290649cee977b04aed8ca8fdcd
Malte Schirmacher <schirmacher@mediamarktsaturn.com>
2024-12-05 08:52:18 +0100

    Fix the empty CDI context

--- src/main/kotlin/org/acme/FooService.kt
+++ src/main/kotlin/org/acme/FooService.kt
@@ -3,14 +3,12 @@ package org.acme
 import io.quarkus.security.identity.CurrentIdentityAssociation
 import io.smallrye.mutiny.coroutines.awaitSuspending
 import jakarta.enterprise.context.ApplicationScoped
-import jakarta.enterprise.context.control.ActivateRequestContext
 import jakarta.ws.rs.ForbiddenException
 
 @ApplicationScoped
 class FooService(
     private val identities: CurrentIdentityAssociation,
 ) {
-    @ActivateRequestContext
     suspend fun doSomething(input: String): String {
         val identity = identities.deferredIdentity.awaitSuspending()
 
--- src/main/kotlin/org/acme/GreetingResource.kt
+++ src/main/kotlin/org/acme/GreetingResource.kt
@@ -3,14 +3,18 @@
 package org.acme
 
 import io.quarkus.security.Authenticated
+import io.quarkus.smallrye.reactivemessaging.runtime.kotlin.VertxDispatcher
 import io.smallrye.mutiny.Uni
 import io.smallrye.mutiny.coroutines.uni
+import io.vertx.core.Vertx
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.PathParam
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.withContext
 
 @Path("/hello")
 @Authenticated
@@ -21,7 +25,16 @@ class GreetingResource(
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/{name}")
-    fun hello(@PathParam("name") name: String): Uni<String> = uni {
-        fooService.doSomething(name)
+    fun hello(@PathParam("name") name: String): Uni<String> {
+
+        val dispatcher: CoroutineDispatcher =
+            Vertx.currentContext()?.let { VertxDispatcher(it) }
+                ?: throw IllegalStateException("No Vertx context found")
+
+        return uni {
+            withContext(dispatcher) {
+                fooService.doSomething(name)
+            }
+        }
     }
 }
