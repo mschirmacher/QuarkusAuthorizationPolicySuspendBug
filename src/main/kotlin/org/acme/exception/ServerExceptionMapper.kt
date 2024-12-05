fbda188e4183c2f17d37c7133573480603080318
Malte Schirmacher <schirmacher@mediamarktsaturn.com>
2024-12-05 16:41:55 +0100

    minor improvement

--- src/main/kotlin/org/acme/exception/ServerExceptionMapper.kt
+++ src/main/kotlin/org/acme/exception/ServerExceptionMapper.kt
@@ -1,6 +1,7 @@
 package org.acme.exception
 
 import com.fasterxml.jackson.core.JsonParseException
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import jakarta.annotation.Priority
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.ws.rs.core.Response
@@ -18,4 +19,9 @@ class ServerExceptionMapper {
     @ServerExceptionMapper
     fun handle(e: ClientWebApplicationException): Response =
         Response.status(234).build()
+
+    @ServerExceptionMapper
+    fun mismatchedInputException(exception: MismatchedInputException) =
+        Response.status(234).build()
+
 }
--- src/test/kotlin/org/acme/GreetingResourceTest.kt
+++ src/test/kotlin/org/acme/GreetingResourceTest.kt
@@ -11,7 +11,6 @@ import io.quarkus.test.junit.QuarkusTest
 import io.quarkus.test.junit.QuarkusTestProfile
 import io.quarkus.test.junit.TestProfile
 import io.restassured.RestAssured.given
-import org.hamcrest.CoreMatchers.`is`
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
